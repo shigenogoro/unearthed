b0d89e557a7f23a78d2fcf3adf282729355c3a74
shigenogoro <kyle990987@gmail.com>
2024-10-02 08:38:07 -0400

    Checkpoint 5

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import giftsRouter from './routes/gifts'
+
+const app = express()
+
+/**
+ * Define Middlewares
+ */
+app.use('/public', express.static('./public'))
+app.use('/scripts', express.static('./public/scripts'))
+app.use('/gifts', giftsRouter)
+
+/**
+ * Define route for the root URL
+ */
+app.get('/', (req, res) => {
+    res.status(200).send('<h1 style="text-align-center; margin-top: 50px;" >UnEarthed API</h1>')
+})
+
+/**
+ * Start a server on port process.env.PORT or 3001
+ */
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`)
+})
